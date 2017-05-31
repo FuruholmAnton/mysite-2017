1f6e1a17c6a117301fd1624fc59f36a2315ab388
Anton Furuholm <anton@buildinamsterdam.com>
2017-05-31 15:41:32 +0200

    Use old settings for now

--- tools/webpack.config.js
+++ tools/webpack.config.js
@@ -330,117 +330,117 @@ const serverConfig = {
 };
 
 
-export default [clientConfig, serverConfig];
+// export default [clientConfig, serverConfig];
 
 
-// module.exports = {
-//   context: path.resolve(__dirname, '../src'),
-//   entry: {
-//     main: [
-//       /* 'babel-polyfill', */
-//       'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000',
-//       './app-client.js',
-//     ],
-//   },
-//   output: {
-//     path: path.resolve(__dirname, '../src/static/'),
-//     filename: 'bundle.js',
-//   },
-//   resolve: {
-//     alias: {
-//       Components: path.resolve(__dirname, '../src/components'),
-//       Pages: path.resolve(__dirname, '../src/pages'),
-//       Layouts: path.resolve(__dirname, '../src/layouts'),
-//       Core: path.resolve(__dirname, '../src/core'),
-//       SVG: path.resolve(__dirname, '../src/assets/svg'),
-//       Images: path.resolve(__dirname, '../src/assets/images'),
-//     },
-//     extensions: ['.js', '.jsx'],
-//     // unsafeCache: true,
-//   },
-//   devtool: 'source-map',
-//   module: {
-//     rules: [{
-//         test: /\.(js|jsx)$/,
-//         include: [
-//           path.resolve(__dirname, '../src'),
-//         ],
-//         exclude: [
-//           path.resolve(__dirname, '../src/static'),
-//           path.resolve(__dirname, '../src/views'),
-//           path.resolve(__dirname, '../src/server.js'),
-//           path.resolve(__dirname, '../src/server.babel.js'),
-//           path.resolve(__dirname, '../src/server*'),
-//           path.resolve(__dirname, '../src/main.js'),
-//         ],
-//         loader: 'babel-loader',
-//         options: {
-//           presets: ['es2015', 'react'],
-//         },
-//       },
-//       {
-//         test: /\.scss$/,
-//         include: [
-//           path.resolve(__dirname, '../src/assets/scss'),
-//           path.resolve(__dirname, '../src/pages'),
-//         ],
-//         loader: extractSass.extract({
-//           fallback: 'style-loader',
-//           use: [
-//             'css-loader',
-//             {
-//               loader: 'postcss-loader',
-//               options: {
-//                 plugins: () => ([
-//                   require('postcss-cssnext')(),
-//                 ]),
-//               },
-//             },
-//             'sass-loader',
-//           ],
-//         }),
-//       },
-//       {
-//         test: webpackIsomorphicToolsPlugin.regular_expression('images'),
-//         loader: 'url-loader?limit=10240', // any image below or equal to 10K will be converted to inline base64 instead
-//       },
-//       {
-//         test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-//         exclude: /(node_modules|bower_components)/,
-//         loader: 'file',
-//       },
-//       {
-//         test: /\.(woff|woff2)$/,
-//         exclude: /(node_modules|bower_components)/,
-//         loader: 'url?prefix=font/&limit=5000',
-//       },
-//       {
-//         test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-//         exclude: /(node_modules|bower_components)/,
-//         loader: 'url?limit=10000&mimetype=application/octet-stream',
-//       },
-//     ],
-//   },
-//   plugins: [
+module.exports = {
+  context: path.resolve(__dirname, '../src'),
+  entry: {
+    main: [
+      /* 'babel-polyfill', */
+      'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000',
+      './app-client.js',
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, '../src/static/'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    alias: {
+      Components: path.resolve(__dirname, '../src/components'),
+      Pages: path.resolve(__dirname, '../src/pages'),
+      Layouts: path.resolve(__dirname, '../src/layouts'),
+      Core: path.resolve(__dirname, '../src/core'),
+      SVG: path.resolve(__dirname, '../src/assets/svg'),
+      Images: path.resolve(__dirname, '../src/assets/images'),
+    },
+    extensions: ['.js', '.jsx'],
+    // unsafeCache: true,
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [{
+        test: /\.(js|jsx)$/,
+        include: [
+          path.resolve(__dirname, '../src'),
+        ],
+        exclude: [
+          path.resolve(__dirname, '../src/static'),
+          path.resolve(__dirname, '../src/views'),
+          path.resolve(__dirname, '../src/server.js'),
+          path.resolve(__dirname, '../src/server.babel.js'),
+          path.resolve(__dirname, '../src/server*'),
+          path.resolve(__dirname, '../src/main.js'),
+        ],
+        loader: 'babel-loader',
+        options: {
+          presets: ['es2015', 'react'],
+        },
+      },
+      {
+        test: /\.scss$/,
+        include: [
+          path.resolve(__dirname, '../src/assets/scss'),
+          path.resolve(__dirname, '../src/pages'),
+        ],
+        loader: extractSass.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader',
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: () => ([
+                  require('postcss-cssnext')(),
+                ]),
+              },
+            },
+            'sass-loader',
+          ],
+        }),
+      },
+      {
+        test: webpackIsomorphicToolsPlugin.regular_expression('images'),
+        loader: 'url-loader?limit=10240', // any image below or equal to 10K will be converted to inline base64 instead
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'file',
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'url?prefix=font/&limit=5000',
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'url?limit=10000&mimetype=application/octet-stream',
+      },
+    ],
+  },
+  plugins: [
 
-//     extractSass,
-//     webpackIsomorphicToolsPlugin,
+    extractSass,
+    webpackIsomorphicToolsPlugin,
 
-//     // new webpack.optimize.OccurenceOrderPlugin(), // webpack 1.x
-//     new webpack.HotModuleReplacementPlugin(),
-//     new webpack.NoEmitOnErrorsPlugin(),
+    // new webpack.optimize.OccurenceOrderPlugin(), // webpack 1.x
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
 
-//     // new webpack.DefinePlugin({
-//     //   'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-//     // }),
-//     // new webpack.optimize.DedupePlugin(),
-//     // new webpack.optimize.OccurenceOrderPlugin(),
-//     // new webpack.optimize.UglifyJsPlugin({
-//     //   compress: { warnings: false },
-//     //   mangle: true,
-//     //   sourcemap: false,
-//     //   beautify: false,
-//     //   dead_code: true
-//     // })
-//   ],
-// };
+    // new webpack.DefinePlugin({
+    //   'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    // }),
+    // new webpack.optimize.DedupePlugin(),
+    // new webpack.optimize.OccurenceOrderPlugin(),
+    // new webpack.optimize.UglifyJsPlugin({
+    //   compress: { warnings: false },
+    //   mangle: true,
+    //   sourcemap: false,
+    //   beautify: false,
+    //   dead_code: true
+    // })
+  ],
+};
